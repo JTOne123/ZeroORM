e59e0369a05091dfd982dcf4880a6ea4dea8e41f
bessgeor <bessgeor@gmail.com>
2018-08-20 02:24:11 +0300

    Add SqlBuilder class

---
+++ src/SqlBuilder.cs
@@ -0,0 +1,12 @@
+﻿using ZeroORM.Extensibility.Metadata;
+
+namespace ZeroORM
+{
+	public sealed class SqlBuilder
+	{
+		private readonly IMetadataProvider _metadata;
+
+		public SqlBuilder( IMetadataProvider metadata )
+			=> _metadata = metadata;
+	}
+}
