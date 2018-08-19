8d0d0f9ed7f5a1532608724f375081644480b0a0
bessgeor <bessgeor@gmail.com>
2018-08-20 02:29:35 +0300

    Expose metadata internally

---
+++ src/ISqlBuilder.cs
@@ -0,0 +1,9 @@
+﻿using ZeroORM.Extensibility.Metadata;
+
+namespace ZeroORM
+{
+	internal interface ISqlBuilder
+	{
+		IMetadataProvider Metadata { get; }
+	}
+}
--- src/SqlBuilder.cs
+++ src/SqlBuilder.cs
@@ -2,11 +2,13 @@
 
 namespace ZeroORM
 {
-	public sealed class SqlBuilder
+	public sealed class SqlBuilder : ISqlBuilder
 	{
 		private readonly IMetadataProvider _metadata;
 
 		public SqlBuilder( IMetadataProvider metadata )
 			=> _metadata = metadata;
+
+		IMetadataProvider ISqlBuilder.Metadata => _metadata;
 	}
 }
