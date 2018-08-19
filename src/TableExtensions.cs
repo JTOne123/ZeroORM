3953fc8afc55f691388b1b63d8f7144bda86d5e2
bessgeor <bessgeor@gmail.com>
2018-08-20 02:31:43 +0300

    Add nice way to extract tables from builder

---
+++ src/TableExtensions.cs
@@ -0,0 +1,13 @@
+﻿using ZeroORM.Extensibility.Metadata;
+
+namespace ZeroORM
+{
+	public static class TableExtensions
+	{
+		public static SqlBuilder UseTable<TEntity>( this SqlBuilder builder, out ITableMetadata<TEntity> table )
+		{
+			table = ( (ISqlBuilder) builder ).Metadata.GetTable<TEntity>();
+			return builder;
+		}
+	}
+}
