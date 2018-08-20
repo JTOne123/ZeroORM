d7b695b39b6295923ab4d4cf966d2f64e573d7de
bessgeor <bessgeor@gmail.com>
2018-08-20 23:21:56 +0300

    Add unit tests project

---
+++ test/QueryExtensionsTests/RawSql.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace ZeroORM.Test.QueryExtensionsTests
+{
+	public class RawSql
+	{
+		private const string _testString = "test";
+
+		[Fact]
+		public void RawSqlReturnsItsParameter()
+			=> ( null as SqlBuilder )
+			.RawSql( _testString )
+			.Should()
+			.BeSameAs( _testString );
+	}
+}
