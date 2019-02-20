60a02663b9bbfc8756236a1704b6ce16a7745761
bessgeor <bessgeor@gmail.com>
2019-02-20 11:54:55 +0300

    Add new exception class ( issue::2 )

---
+++ src/Exceptions/PropertyIsNotCallableException.cs
@@ -0,0 +1,11 @@
+﻿namespace ZeroORM.Exceptions
+{
+	public class PropertyIsNotCallableException : ZeroORMException
+	{
+		public PropertyIsNotCallableException(string subMessage)
+			: base($"Property is not intended to be accessed directly (not from AST). {subMessage}")
+		{
+
+		}
+	}
+}
