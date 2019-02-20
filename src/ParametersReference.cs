115e0fe1e8b86d70b763e9e6b8f1345bf2159c6c
bessgeor <bessgeor@gmail.com>
2019-02-20 13:39:30 +0300

    Add the same struct for parameters ( issue::2 )

---
+++ src/ParametersReference.cs
@@ -0,0 +1,16 @@
+﻿using ZeroORM.Exceptions;
+
+namespace ZeroORM
+{
+	public readonly struct ParametersReference<TEntity> : System.IEquatable<ParametersReference<TEntity>>
+	{
+		public TEntity Values => throw new PropertyIsNotCallableException($"ZeroORM.ParametersReference<{typeof(TEntity).FullName}>.Values");
+
+		public override int GetHashCode() => 0;
+		public bool Equals( ParametersReference<TEntity> other ) => true;
+		public override bool Equals( object obj ) => obj is ParametersReference<TEntity> other && Equals( other );
+
+		public static bool operator ==( ParametersReference<TEntity>x, ParametersReference<TEntity>y ) => x.Equals( y );
+		public static bool operator !=( ParametersReference<TEntity>x, ParametersReference<TEntity>y ) => !x.Equals( y );
+	}
+}
