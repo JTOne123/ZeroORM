b7b1e6e0f374fb555652775ec9433c718cb07a5b
bessgeor <bessgeor@gmail.com>
2019-03-03 12:26:33 +0300

    temp ( issue::2 )

---
+++ src/Infrastructure/ExpressionBasedSqlAssembler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace ZeroORM.Infrastructure
+{
+	internal static class ExpressionBasedSqlAssembler
+	{
+		private class UnderhoodVisitor : ExpressionVisitor
+		{
+			private bool IsReference( Type t )
+				=> t.IsGenericType
+				&& t.GetGenericTypeDefinition() == typeof( TableReference<> )
+				;
+
+			protected override Expression VisitUnary( UnaryExpression node )
+			{
+				if (node.NodeType == ExpressionType.Convert)
+				{
+
+				}
+				return base.VisitUnary( node );
+			}
+
+			protected override Expression VisitMember( MemberExpression node )
+			{
+				Type sourceType = node.Expression.Type;
+				if ( !IsReference( sourceType ) )
+					if ( node.Expression is MemberExpression member && IsReference( member.Expression.Type ) )
+						sourceType = member.Expression.Type;
+					else
+						return base.VisitMember( node );
+
+			}
+		}
+
+		public static string ToSql( Expression<Func<FormattableString>> expression )
+		{
+
+		}
+	}
+}
