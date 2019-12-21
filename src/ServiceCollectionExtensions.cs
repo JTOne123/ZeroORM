98e69b1281cbba9eb720a6966280c84bd765ed14
bessgeor <bessgeor@gmail.com>
2019-12-22 01:16:56 +0300

    temp ( issue::1 )

---
+++ src/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZeroORM
+{
+	public static class ServiceCollectionExtensions
+	{
+		public static IServiceCollection UseZeroOrm(object t)
+		{
+			object x = new
+			{
+				X = 48
+			};
+		}
+	}
+}
